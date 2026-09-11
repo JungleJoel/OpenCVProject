5647f1112e351705ba0a6ce716899769f28ebaeb
Joel Seger <joel.allarp@gmail.com>
2026-09-11 10:24:09 +0200

    First commit

---
+++ build/CMakeFiles/4.3.4/CMakeCXXCompiler.cmake
@@ -0,0 +1,102 @@
+set(CMAKE_CXX_COMPILER "/nix/store/w88q44gqd1qg5wmkk7v0h97rpiqvam0l-gcc-wrapper-15.3.0/bin/g++")
+set(CMAKE_CXX_COMPILER_ARG1 "")
+set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_VERSION "15.3.0")
+set(CMAKE_CXX_COMPILER_VERSION_INTERNAL "")
+set(CMAKE_CXX_COMPILER_WRAPPER "")
+set(CMAKE_CXX_STANDARD_COMPUTED_DEFAULT "17")
+set(CMAKE_CXX_EXTENSIONS_COMPUTED_DEFAULT "ON")
+set(CMAKE_CXX_STANDARD_LATEST "26")
+set(CMAKE_CXX_COMPILE_FEATURES "cxx_std_98;cxx_template_template_parameters;cxx_std_11;cxx_alias_templates;cxx_alignas;cxx_alignof;cxx_attributes;cxx_auto_type;cxx_constexpr;cxx_decltype;cxx_decltype_incomplete_return_types;cxx_default_function_template_args;cxx_defaulted_functions;cxx_defaulted_move_initializers;cxx_delegating_constructors;cxx_deleted_functions;cxx_enum_forward_declarations;cxx_explicit_conversions;cxx_extended_friend_declarations;cxx_extern_templates;cxx_final;cxx_func_identifier;cxx_generalized_initializers;cxx_inheriting_constructors;cxx_inline_namespaces;cxx_lambdas;cxx_local_type_template_args;cxx_long_long_type;cxx_noexcept;cxx_nonstatic_member_init;cxx_nullptr;cxx_override;cxx_range_for;cxx_raw_string_literals;cxx_reference_qualified_functions;cxx_right_angle_brackets;cxx_rvalue_references;cxx_sizeof_member;cxx_static_assert;cxx_strong_enums;cxx_thread_local;cxx_trailing_return_types;cxx_unicode_literals;cxx_uniform_initialization;cxx_unrestricted_unions;cxx_user_literals;cxx_variadic_macros;cxx_variadic_templates;cxx_std_14;cxx_aggregate_default_initializers;cxx_attribute_deprecated;cxx_binary_literals;cxx_contextual_conversions;cxx_decltype_auto;cxx_digit_separators;cxx_generic_lambdas;cxx_lambda_init_captures;cxx_relaxed_constexpr;cxx_return_type_deduction;cxx_variable_templates;cxx_std_17;cxx_std_20;cxx_std_23;cxx_std_26")
+set(CMAKE_CXX98_COMPILE_FEATURES "cxx_std_98;cxx_template_template_parameters")
+set(CMAKE_CXX11_COMPILE_FEATURES "cxx_std_11;cxx_alias_templates;cxx_alignas;cxx_alignof;cxx_attributes;cxx_auto_type;cxx_constexpr;cxx_decltype;cxx_decltype_incomplete_return_types;cxx_default_function_template_args;cxx_defaulted_functions;cxx_defaulted_move_initializers;cxx_delegating_constructors;cxx_deleted_functions;cxx_enum_forward_declarations;cxx_explicit_conversions;cxx_extended_friend_declarations;cxx_extern_templates;cxx_final;cxx_func_identifier;cxx_generalized_initializers;cxx_inheriting_constructors;cxx_inline_namespaces;cxx_lambdas;cxx_local_type_template_args;cxx_long_long_type;cxx_noexcept;cxx_nonstatic_member_init;cxx_nullptr;cxx_override;cxx_range_for;cxx_raw_string_literals;cxx_reference_qualified_functions;cxx_right_angle_brackets;cxx_rvalue_references;cxx_sizeof_member;cxx_static_assert;cxx_strong_enums;cxx_thread_local;cxx_trailing_return_types;cxx_unicode_literals;cxx_uniform_initialization;cxx_unrestricted_unions;cxx_user_literals;cxx_variadic_macros;cxx_variadic_templates")
+set(CMAKE_CXX14_COMPILE_FEATURES "cxx_std_14;cxx_aggregate_default_initializers;cxx_attribute_deprecated;cxx_binary_literals;cxx_contextual_conversions;cxx_decltype_auto;cxx_digit_separators;cxx_generic_lambdas;cxx_lambda_init_captures;cxx_relaxed_constexpr;cxx_return_type_deduction;cxx_variable_templates")
+set(CMAKE_CXX17_COMPILE_FEATURES "cxx_std_17")
+set(CMAKE_CXX20_COMPILE_FEATURES "cxx_std_20")
+set(CMAKE_CXX23_COMPILE_FEATURES "cxx_std_23")
+set(CMAKE_CXX26_COMPILE_FEATURES "cxx_std_26")
+
+set(CMAKE_CXX_PLATFORM_ID "Linux")
+set(CMAKE_CXX_SIMULATE_ID "")
+set(CMAKE_CXX_COMPILER_FRONTEND_VARIANT "GNU")
+set(CMAKE_CXX_COMPILER_APPLE_SYSROOT "")
+set(CMAKE_CXX_SIMULATE_VERSION "")
+set(CMAKE_CXX_COMPILER_ARCHITECTURE_ID "x86_64")
+
+
+
+
+set(CMAKE_AR "/nix/store/w88q44gqd1qg5wmkk7v0h97rpiqvam0l-gcc-wrapper-15.3.0/bin/ar")
+set(CMAKE_CXX_COMPILER_AR "/nix/store/4yxdxw3kppr2pbic95p4mri520rkdag2-gcc-15.3.0/bin/gcc-ar")
+set(CMAKE_RANLIB "/nix/store/w88q44gqd1qg5wmkk7v0h97rpiqvam0l-gcc-wrapper-15.3.0/bin/ranlib")
+set(CMAKE_CXX_COMPILER_RANLIB "/nix/store/4yxdxw3kppr2pbic95p4mri520rkdag2-gcc-15.3.0/bin/gcc-ranlib")
+set(CMAKE_LINKER "/nix/store/w88q44gqd1qg5wmkk7v0h97rpiqvam0l-gcc-wrapper-15.3.0/bin/ld")
+set(CMAKE_LINKER_LINK "")
+set(CMAKE_LINKER_LLD "")
+set(CMAKE_CXX_COMPILER_LINKER "/nix/store/rm982p6i9ry7rwq5jn638kdpv0a2331f-binutils-wrapper-2.46/bin/ld")
+set(CMAKE_CXX_COMPILER_LINKER_ID "GNU")
+set(CMAKE_CXX_COMPILER_LINKER_VERSION 2.46)
+set(CMAKE_CXX_COMPILER_LINKER_FRONTEND_VARIANT GNU)
+set(CMAKE_MT "")
+set(CMAKE_TAPI "CMAKE_TAPI-NOTFOUND")
+set(CMAKE_COMPILER_IS_GNUCXX 1)
+set(CMAKE_CXX_COMPILER_LOADED 1)
+set(CMAKE_CXX_COMPILER_WORKS TRUE)
+set(CMAKE_CXX_ABI_COMPILED TRUE)
+
+set(CMAKE_CXX_COMPILER_ENV_VAR "CXX")
+
+set(CMAKE_CXX_COMPILER_ID_RUN 1)
+set(CMAKE_CXX_SOURCE_FILE_EXTENSIONS C;M;c++;cc;cpp;cxx;m;mm;mpp;CPP;ixx;cppm;ccm;cxxm;c++m)
+set(CMAKE_CXX_IGNORE_EXTENSIONS inl;h;hpp;HPP;H;o;O;obj;OBJ;def;DEF;rc;RC)
+
+foreach (lang IN ITEMS C OBJC OBJCXX)
+  if (CMAKE_${lang}_COMPILER_ID_RUN)
+    foreach(extension IN LISTS CMAKE_${lang}_SOURCE_FILE_EXTENSIONS)
+      list(REMOVE_ITEM CMAKE_CXX_SOURCE_FILE_EXTENSIONS ${extension})
+    endforeach()
+  endif()
+endforeach()
+
+set(CMAKE_CXX_LINKER_PREFERENCE 30)
+set(CMAKE_CXX_LINKER_PREFERENCE_PROPAGATES 1)
+set(CMAKE_CXX_LINKER_DEPFILE_SUPPORTED TRUE)
+set(CMAKE_LINKER_PUSHPOP_STATE_SUPPORTED TRUE)
+set(CMAKE_CXX_LINKER_PUSHPOP_STATE_SUPPORTED TRUE)
+
+# Save compiler ABI information.
+set(CMAKE_CXX_SIZEOF_DATA_PTR "8")
+set(CMAKE_CXX_COMPILER_ABI "ELF")
+set(CMAKE_CXX_BYTE_ORDER "LITTLE_ENDIAN")
+set(CMAKE_CXX_LIBRARY_ARCHITECTURE "")
+
+if(CMAKE_CXX_SIZEOF_DATA_PTR)
+  set(CMAKE_SIZEOF_VOID_P "${CMAKE_CXX_SIZEOF_DATA_PTR}")
+endif()
+
+if(CMAKE_CXX_COMPILER_ABI)
+  set(CMAKE_INTERNAL_PLATFORM_ABI "${CMAKE_CXX_COMPILER_ABI}")
+endif()
+
+if(CMAKE_CXX_LIBRARY_ARCHITECTURE)
+  set(CMAKE_LIBRARY_ARCHITECTURE "")
+endif()
+
+set(CMAKE_CXX_CL_SHOWINCLUDES_PREFIX "")
+if(CMAKE_CXX_CL_SHOWINCLUDES_PREFIX)
+  set(CMAKE_CL_SHOWINCLUDES_PREFIX "${CMAKE_CXX_CL_SHOWINCLUDES_PREFIX}")
+endif()
+
+
+
+
+
+set(CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES "/nix/store/7ydf0dzbsq7w35bxifci2mslm76pw1qf-gnumake-4.4.1/include;/nix/store/7pcsxbv9grrppshv23qq1iknpb9y5qlk-opencv-4.13.0/include;/nix/store/r8h5bvi8sa7k0swb3b2byps8d3d8rv2v-gtk+-2.24.33-dev/include;/nix/store/p0ijpx5x5kppx8jrpqzk5zk6ksfw55vl-at-spi2-core-2.60.6-dev/include;/nix/store/s30pndvs4kh1dk7jxxj4ljn27spyr8fv-dbus-1.16.2-dev/include;/nix/store/nxxysnlnrgvg42h8fz9zlmsdiixmw4zj-glib-2.88.3-dev/include;/nix/store/nd34kmibbq6y7qmp78rhw38adsj5ffd0-zlib-1.3.2-dev/include;/nix/store/ysxsfbvgz35w24g5c74604rhxvmia47v-libffi-3.8.0-dev/include;/nix/store/518bcx82g8m9cyji47sprx9ajmadjk8s-gettext-1.0/include;/nix/store/m5nwyps66vgzkqk9vnh7yr9ly7z7hhi3-glibc-iconv-2.42/include;/nix/store/j8aysi64y93f62rxvf3x2i0h1l299h28-cairo-1.18.4-dev/include;/nix/store/vwlb36mv7jbar7y76l7kilvln04xqf6n-fontconfig-2.18.2-dev/include;/nix/store/z203ci88037mkq16c1kn8gy518m3ab1y-freetype-2.14.3-dev/include;/nix/store/9vxrnnhc400s1rgq801wqfdxhzpcl1z1-bzip2-1.0.8-dev/include;/nix/store/46hh240hlj9si28zra66dyc3nj85lynw-brotli-1.2.0-dev/include;/nix/store/p8m024sxqwg7il6660zqgshrqpsvf43l-libpng-apng-1.6.58-dev/include;/nix/store/iss09vixlpbq7jwf2y5wkmxvcfydgm3v-expat-2.8.3-dev/include;/nix/store/8kmh5aaiadz1vfv1nal7vf3x41b2l92j-pixman-0.46.4/include;/nix/store/p7qxr05x9dzb0xxxss8iq73vaz8wjzwp-libxext-1.3.7-dev/include;/nix/store/y4k7f46xqci803kfbi78sfy09r1a978x-xorgproto-2025.1/include;/nix/store/m9n4kl4gv4qp5kyz334fqhk2p9qamljw-libxau-1.0.12-dev/include;/nix/store/0p5nsfpza8g92hnrmrm1rqvm25l1mlgv-libxrender-0.9.12-dev/include;/nix/store/4sh8xds6acpkvb2f1dqh0jsgvgn5p2sy-libx11-1.8.13-dev/include;/nix/store/b3wyx65651qsp5v73kiyljnv5hr4dgv3-libxcb-1.17.0-dev/include;/nix/store/pqqqjfb1pgggrljcl0q9fk3alm2b0kh7-gdk-pixbuf-2.44.7-dev/include;/nix/store/8lyz7iilgz3a1jlg4qf02w0xizzmd52i-libtiff-4.7.2-dev/include;/nix/store/21np1b2yflzayldrsy3zp2gg5snhkgwz-libjpeg-turbo-3.1.4.1-dev/include;/nix/store/ghks3c4bpqll9b1909vr47qx892c3zzw-zstd-1.5.7-dev/include;/nix/store/2f0xdvgl9csjsc2yhnfpgm6qyzcypwxl-libdeflate-1.25/include;/nix/store/gbzc2cpjmhylnmwljbqa1pwmli5a2n1v-xz-5.8.3-dev/include;/nix/store/bwaay526pyr0wf3wksskivgxzriyrmld-libwebp-1.6.0/include;/nix/store/8sv07mfqjbbhb0lac3bn2l0qyigafjzi-lerc-4.2.0-dev/include;/nix/store/5848cfwnrv0by7nb25swhzysd73z9mgc-pango-1.57.1-dev/include;/nix/store/gcvhp35cil7kncli38xlgbk5zy6d7j07-harfbuzz-13.2.1-dev/include;/nix/store/lq0l3zkbnb1rhrwbgnr2w0svjr8wyc4r-graphite2-1.3.15-dev/include;/nix/store/fd8hf26hs7hnpvdk4vrfynrc3y5mwvm4-libxft-2.3.9-dev/include;/nix/store/gl601sbj8md9f4gn59g43f4rg7fyh0b7-libxcomposite-0.4.7-dev/include;/nix/store/xm0lc6hwnczyhm9y23gqdc4ixpzalxji-libxfixes-6.0.2-dev/include;/nix/store/b3v2anf0jhzgapfpj5pc9z9x2jmk7l80-libxcursor-1.2.3-dev/include;/nix/store/gm8wkz37gxc0xiyf48k28c0n2a0bv0rs-libxi-1.8.3-dev/include;/nix/store/h0i1gqjmnwb06h30sdpr62vk2mcvsixy-libxrandr-1.5.5-dev/include;/nix/store/b8ibmndpzni0idlfr1crb200y6rkvfby-libxinerama-1.1.6-dev/include;/nix/store/fsfrc92iamihmgj1ar6zxd2inynp2l66-cups-2.4.19-dev/include;/nix/store/wjfai7nny77igkc51xj1izpz2gz6j366-gmp-with-cxx-6.3.0-dev/include;/nix/store/iwiw531gwyp19rcn270jgmngxqa23gvy-ffmpeg-9.0.1-dev/include;/nix/store/b5bpi6zfajzzrwwpgba2q6li3nnya4bs-python3-3.14.7/include;/nix/store/bg2bp1zmii5spb2hkgv5xn6mzva7nx9r-gdb-17.2/include;/nix/store/3qasfqdawbh8c9fzagx3dfs1xdn7vba6-lldb-21.1.8-dev/include;/nix/store/5jhgdsgl541jznvgv2xlhhksnkix2hib-valgrind-3.27.1-dev/include;/nix/store/g5l3mkk656ccm58gzz0a9kryy2grih01-compiler-rt-libc-21.1.8-dev/include;/nix/store/4yxdxw3kppr2pbic95p4mri520rkdag2-gcc-15.3.0/include/c++/15.3.0;/nix/store/4yxdxw3kppr2pbic95p4mri520rkdag2-gcc-15.3.0/include/c++/15.3.0/x86_64-unknown-linux-gnu;/nix/store/4yxdxw3kppr2pbic95p4mri520rkdag2-gcc-15.3.0/include/c++/15.3.0/backward;/nix/store/4yxdxw3kppr2pbic95p4mri520rkdag2-gcc-15.3.0/lib/gcc/x86_64-unknown-linux-gnu/15.3.0/include;/nix/store/4yxdxw3kppr2pbic95p4mri520rkdag2-gcc-15.3.0/include;/nix/store/4yxdxw3kppr2pbic95p4mri520rkdag2-gcc-15.3.0/lib/gcc/x86_64-unknown-linux-gnu/15.3.0/include-fixed;/nix/store/qwi863qp79xkdm2yw3xzjh73dpg9qh67-glibc-2.42-84-dev/include")
+set(CMAKE_CXX_IMPLICIT_LINK_LIBRARIES "stdc++;m;gcc_s;gcc;c;gcc_s;gcc")
+set(CMAKE_CXX_IMPLICIT_LINK_DIRECTORIES "/nix/store/7pcsxbv9grrppshv23qq1iknpb9y5qlk-opencv-4.13.0/lib;/nix/store/yn8y98bfwgr9jxcrl1x32h04mshpn9wc-dbus-1.16.2-lib/lib;/nix/store/l3p6y554mkxi4vipnnp8809kxj0g1lyv-zlib-1.3.2/lib;/nix/store/wb232z007b8qynidq89lli8lb2c4q0r4-libffi-3.8.0/lib;/nix/store/518bcx82g8m9cyji47sprx9ajmadjk8s-gettext-1.0/lib;/nix/store/a3hr0l5skscvbkcr7kz3nhi4linz1p71-glib-2.88.3/lib;/nix/store/iw5kcyxqr3c4w4m2d2yaqxkkis07awr3-at-spi2-core-2.60.6/lib;/nix/store/xz5xsq0nihfs15fbknz5w0yblg04760q-bzip2-1.0.8/lib;/nix/store/c56x66gbj3qwv3g7dhadcfiq408q2hay-brotli-1.2.0-lib/lib;/nix/store/d3f7r44z2px98z479jgf7qdwqn96jgf6-libpng-apng-1.6.58/lib;/nix/store/dczkv6755chri9gsvacxvqpz9nn2dwld-freetype-2.14.3/lib;/nix/store/2gkqj0v0msfcagwf5ix199rf1lnwf2a1-expat-2.8.3/lib;/nix/store/iv0yvsywa1v35xzdkqz9wdzhg7vshpdd-fontconfig-2.18.2-lib/lib;/nix/store/8kmh5aaiadz1vfv1nal7vf3x41b2l92j-pixman-0.46.4/lib;/nix/store/qdzn5piljhvzcjwxkxfyi0bc0m215wdb-libxau-1.0.12/lib;/nix/store/m51xyp95w4gnhcbncz6xi0x5519m293i-libxext-1.3.7/lib;/nix/store/3vlnkr6z7hh86rrn087df45s4cfjxy65-libx11-1.8.13/lib;/nix/store/avkwd1sfvr31c5ffd06qj0grvyp9pb2w-libxrender-0.9.12/lib;/nix/store/j5ml0bwadkmxc6185ilixlw85ywagxgv-libxcb-1.17.0/lib;/nix/store/imkw2kilahmpk5c2n0img67wq8ij717v-cairo-1.18.4/lib;/nix/store/nc95dy5ln9slqmw1s151mk30ks5f6na6-libjpeg-turbo-3.1.4.1/lib;/nix/store/xcwclh5qgw2x6qc39x6ixara06xczzlw-zstd-1.5.7/lib;/nix/store/2f0xdvgl9csjsc2yhnfpgm6qyzcypwxl-libdeflate-1.25/lib;/nix/store/bdwsskqikgdlq94xqh5dvyd8263w995g-xz-5.8.3/lib;/nix/store/bwaay526pyr0wf3wksskivgxzriyrmld-libwebp-1.6.0/lib;/nix/store/3rynmgnsaz28k69zs2gfqwqnz0ayps5c-lerc-4.2.0/lib;/nix/store/66fky3kgvwffycw067rb7yih282ibzzs-libtiff-4.7.2/lib;/nix/store/9mlc8y3ykj14p4nwadw93g4s8zgw4skn-gdk-pixbuf-2.44.7/lib;/nix/store/r3k50gmygh9r5jbdz2a402aszav2zd7d-graphite2-1.3.15/lib;/nix/store/bn8sa8p2ss2y03iwsyy5w6nlzmfd9avg-harfbuzz-13.2.1/lib;/nix/store/za163pxnk1r2n8rrajqby7qiv5ha38cb-libxft-2.3.9/lib;/nix/store/65582sxlfkllmxwp1l4dwbsfzymzyxry-pango-1.57.1/lib;/nix/store/i4f1ydn42hii1y1sz1wx2pl6lkqyby1l-libxfixes-6.0.2/lib;/nix/store/n9p41ljslm8qcpw26mwh9spw75bi0wsm-libxcomposite-0.4.7/lib;/nix/store/algvsjnffll23bmirbg4a6aycfavk609-libxcursor-1.2.3/lib;/nix/store/kvg5r55y17ybrs2gccjbj4vdg9sp69ia-libxi-1.8.3/lib;/nix/store/jvmx7p97pjyz4a67001lg9yhk0g7qbfm-libxrandr-1.5.5/lib;/nix/store/fvjava8j30snn73swinnx10x8c6s8sy7-libxinerama-1.1.6/lib;/nix/store/jwyskbj2mc7izs67ib2msydsh0vbnwl9-gmp-with-cxx-6.3.0/lib;/nix/store/vjaz8yglcqmbihslm7qj5gkrdz7cd3hi-cups-2.4.19-lib/lib;/nix/store/zka00fyzqg7l26n302ykq8m3xnmknlyy-gtk+-2.24.33/lib;/nix/store/vw6jkg2ilvhhdab70a6hg0pmjw91sf8a-ffmpeg-9.0.1-lib/lib;/nix/store/2y91v9adfb6kyrwzkjix0j9yx78xmyy0-clang-tools-21.1.8/lib;/nix/store/b5bpi6zfajzzrwwpgba2q6li3nnya4bs-python3-3.14.7/lib;/nix/store/bg2bp1zmii5spb2hkgv5xn6mzva7nx9r-gdb-17.2/lib;/nix/store/zxh0xd8lgdgq872vywkvfac1w0flc8w0-lldb-21.1.8/lib;/nix/store/k88rn22mdzqaqmqvkx15dxkw8vy89pc7-bear-4.0.4/lib;/nix/store/n51dhmdbik1kfrsm62j5knavmigwrl1a-glibc-2.42-84/lib;/nix/store/4yxdxw3kppr2pbic95p4mri520rkdag2-gcc-15.3.0/lib/gcc/x86_64-unknown-linux-gnu/15.3.0;/nix/store/0vqb1mcas5j8dv6bhbrshinlgsg6bvgi-gcc-15.3.0-lib/lib;/nix/store/4yxdxw3kppr2pbic95p4mri520rkdag2-gcc-15.3.0/lib")
+set(CMAKE_CXX_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
+set(CMAKE_CXX_COMPILER_CLANG_RESOURCE_DIR "")
+
+set(CMAKE_CXX_COMPILER_IMPORT_STD "")
+set(CMAKE_CXX_COMPILER_IMPORT_STD_ERROR_MESSAGE  "Unsupported generator: Unix Makefiles")
+set(CMAKE_CXX_STDLIB_MODULES_JSON "")
